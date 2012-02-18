c297ce6cebb1ebad1ed85c9f1370326b0d0e39ea
Tony Day <tony@lazylabs.com>
2012-02-18 20:35:59 +1300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ruby on Rails Tutorial: second application
+## Demo App
+
+This is the second application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e135dd89e0806996f6cd1733688e39f4f088ab0fdf0e8c90311767d8ac5885d990e26c0e1e50e8cf5fc39003bed683f67f204595405dd0d2437db0db406c7037'
